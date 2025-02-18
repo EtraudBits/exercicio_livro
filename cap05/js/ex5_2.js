ad61aca861fa4821aa419015cea7d7d8522022b4
duarte-oliveira <du84arte@gmail.com>
2025-02-18 20:24:17 -0300

    inclusão ex5_2

---
+++ cap05/js/ex5_2.js
@@ -0,0 +1,18 @@
+//cria referencia aos elementos da pagina
+const frm = document.querySelector("form")
+const resp = document.querySelector("h3")
+
+//cria "ouvinte" de evento, acionado quando o botão submit for clicado
+frm.addEventListener ("submit", (e) => {
+    e.preventDefault() //evita o form ser enviado
+    //obtém conteudo dos campos de entrada
+    const numero = Number(frm.inNumero.value)
+
+    //string para montar a resposta
+    let resposta = `Entre ${numero} e 1: `
+    //cria um for decrescente
+    for (let i = numero; i > 1; i--) {
+        resposta = resposta + i + ", " //resposta acumula números (e virgulas)
+    }
+    resp.innerText = resposta + "1." //exibe a resposta
+})
